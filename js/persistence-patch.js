aaa4d227a013853dac12d9aa027c72f7512c8982
forderdev <gezgintviletisim@gmail.com>
2026-08-19 03:33:26 +0300

    Enable memory-efficient raster history

--- js/persistence-patch.js
+++ js/persistence-patch.js
@@ -3,6 +3,7 @@ import './qa-final.js';
 import './material-groups-patch.js';
 import './document-state-patch.js';
 import './history-patch.js';
+import './raster-history-patch.js';
 import { TextureEditor } from './texture-editor.js';
 
 const originalLoadImageSource = TextureEditor.prototype.loadImageSource;
